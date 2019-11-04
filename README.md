9f1ae72b1d2c49a2ee6f9821bb0834a31394d277
Linus Örnborg <linus.skola@ornborg.se>
2019-11-04 16:53:51 +0100

    edited readme & index

--- README.md
+++ README.md
@@ -2,6 +2,8 @@
   <img src="https://www.pinclipart.com/picdir/big/13-134576_stock-market-monster-clip-art-scary-monster-png.png" width="300">
   <h1 align="center">A tale to learn by Linus, Carl, Anton, Khaled & Jonathan</h1>
   <p align="center">This is a unique tale about a very big monster called Bad Mood.<p>
+  <p align="center">Read the complete tale here:<p>
+  <p align="center">https://linus-skola.github.io/a-tale-to-learn-by-linus-ornborg-sut19/<p>
 </p>
 
   <br>
--- assets/txtContent.js
+++ assets/txtContent.js
@@ -7,33 +7,37 @@
 var request = new XMLHttpRequest()
 request.open('GET', 'https://api.github.com/repos/linus-skola/a-tale-to-learn-by-linus-ornborg-sut19/contents/', true)
 request.onload = function() {
+
   // Begin accessing JSON data here
   var data = JSON.parse(this.response)
   if (request.status >= 200 && request.status < 400){
     data.forEach(repo => {
         var url = repo.download_url;
-        const content = repo.name;
-        var txt = content;
-        if(content.endsWith(".txt")){
+        const fileName = repo.name;
+        var txt = fileName;
+
+        if(fileName.endsWith(".txt")){
+            console.log(fileName);
 
             var xhttp;
             xhttp=new XMLHttpRequest();
             xhttp.onreadystatechange = function() {
+                
+                if (this.readyState == 4 && this.status == 200) {
+                    console.log(fileName);
+                    const app = document.getElementById('one')
+                
+                    const name = document.createElement('p')
+                    name.textContent = txt;
+                    const content = document.createElement('p')
+                    content.setAttribute('align', 'center')
+                    content.textContent = xhttp.responseText;
 
-            if (this.readyState == 4 && this.status == 200) {
+                    app.appendChild(name)
+                    app.appendChild(content)
 
-                const app = document.getElementById('one')
-            
-                const name = document.createElement('p')
-                name.textContent = txt;
-                const content = document.createElement('p')
-                content.setAttribute('align', 'center')
-                content.textContent = xhttp.responseText;
-
-                app.appendChild(name)
-                app.appendChild(content)
+                }
             }
-        };
         xhttp.open("GET", url, true);
         xhttp.send();
 
