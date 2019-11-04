4faeabc4f556e7c4b266e90ee005e29d974879fc
Linus Örnborg <linus.skola@ornborg.se>
2019-11-04 14:00:54 +0100

    test

--- assets/getContent.js
+++ assets/getContent.js
@@ -1,10 +1,6 @@
 const app = document.getElementById('one')
 
-<<<<<<< HEAD
-var url = "https://raw.githubusercontent.com/linus-skola/a-tale-to-learn-by-linus-ornborg-sut19/master/9_end.txt";
-=======
 var url = "https://raw.githubusercontent.com/linus-skola/a-tale-to-learn-by-linus-ornborg-sut19/master/1_tale_begins.txt";
->>>>>>> linus
 var xhttp;
 xhttp=new XMLHttpRequest();
 xhttp.onreadystatechange = function() {
@@ -21,9 +17,5 @@ function myFunction(xhttp) {
     content.textContent = xhttp.responseText;
 
     app.appendChild(content)
-<<<<<<< HEAD
-    console.log(content)
-=======
->>>>>>> linus
 
   }
